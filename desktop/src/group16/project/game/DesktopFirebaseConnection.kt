5846e3dd8e4b5113d283160a933f33cf15d6d830
kavusiks <kavusikan1998@gmail.com>
2022-03-19 23:01:39 +0100

    (#8) Added the GDX Fireapp api

--- desktop/src/group16/project/game/DesktopFirebaseConnection.kt
+++ desktop/src/group16/project/game/DesktopFirebaseConnection.kt
@@ -1,12 +1,31 @@
 package group16.project.game
 
 import group16.project.game.models.FirebaseInterface
+import pl.mk5.gdx.fireapp.GdxFIRApp
+import pl.mk5.gdx.fireapp.GdxFIRAuth
+import pl.mk5.gdx.fireapp.GdxFIRDatabase
+
+//import
 
 class DesktopFirebaseConnection : FirebaseInterface {
 
+    init {
+        //TODO: Error hher
+    println(GdxFIRApp.inst().configure());
+
+    }
+
+
     override fun someFunction() {
         //TODO("Not yet implemented")
-        println("Desktop conenction class")
+        println("Desktop connection class")
+        GdxFIRAuth.inst().signInAnonymously()
+            .then(
+                GdxFIRDatabase.inst()
+                    .inReference("message")
+                    .setValue("Worked from dekstop!")
+            )
+
     }
 
     override fun test() {
