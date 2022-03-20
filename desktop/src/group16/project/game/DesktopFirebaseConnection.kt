782f0f9088cbdffaae8b9326509bdf6677879d93
kavusiks <kavusikan1998@gmail.com>
2022-03-20 19:06:24 +0100

    (#8) Added comments in the code

--- desktop/src/group16/project/game/DesktopFirebaseConnection.kt
+++ desktop/src/group16/project/game/DesktopFirebaseConnection.kt
@@ -4,35 +4,20 @@ import group16.project.game.models.FirebaseInterface
 import pl.mk5.gdx.fireapp.GdxFIRApp
 import pl.mk5.gdx.fireapp.GdxFIRAuth
 import pl.mk5.gdx.fireapp.GdxFIRDatabase
+import pl.mk5.gdx.fireapp.GdxFIRLogger
+import pl.mk5.gdx.fireapp.auth.GdxFirebaseUser
 
 //import
-
+/**
+ * TODO
+ * We need to find out if we need the desktop version.
+ * -If so we need to create at way to interact with the firebase project.
+ * -If not, we need to do some changes in core, remove this class, etc
+ */
 class DesktopFirebaseConnection : FirebaseInterface {
 
-    init {
-        //TODO: Error hher
-    println(GdxFIRApp.inst().configure());
-
-    }
-
-
-    override fun someFunction() {
+    override fun setValueInDb(target: String, value: String) {
         //TODO("Not yet implemented")
         println("Desktop connection class")
-        GdxFIRAuth.inst().signInAnonymously()
-            .then(
-                GdxFIRDatabase.inst()
-                    .inReference("message")
-                    .setValue("Worked from dekstop!")
-            )
-
-    }
-
-    override fun test() {
-        TODO("Not yet implemented")
-    }
-
-    override fun setValueInDb(target: String, value: String) {
-        TODO("Not yet implemented")
     }
 }
--- desktop/src/group16/project/game/DesktopLauncher.kt
+++ desktop/src/group16/project/game/DesktopLauncher.kt
@@ -5,11 +5,13 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import group16.project.game.StarBattle
 import group16.project.game.models.CoreFirebaseConnection
+import pl.mk5.gdx.fireapp.GdxFIRApp
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
+        //GdxFIRApp.inst().configure();
         Lwjgl3Application(StarBattle(DesktopFirebaseConnection()), Lwjgl3ApplicationConfiguration().apply {
             setForegroundFPS(60)
             useVsync(true)
